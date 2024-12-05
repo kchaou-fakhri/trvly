8fa2ca23ba6ab02ae633da6dcef6e36af447f6d1
kchaou-fakhri <kchaoufakhri@gmail.com>
2024-12-05 15:28:13 +0100

    feat: patch react-native-i18n package and add device language hook

--- src/hooks/useDeviceLangauge.ts
+++ src/hooks/useDeviceLangauge.ts
@@ -1,1 +1,24 @@
-export const useDeviceLanguage = () => {};
+import {useEffect, useState} from 'react';
+import {NativeModules, Platform} from 'react-native';
+
+export const useDeviceLanguage = () => {
+  const [language, setLanguage] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getDeviceLanguage = () => {
+      let deviceLanguage: string;
+      if (Platform.OS === 'ios') {
+        deviceLanguage =
+          NativeModules.SettingsManager.settings.AppleLocale ||
+          NativeModules.SettingsManager.settings.AppleLanguages[0];
+      } else {
+        deviceLanguage = NativeModules.I18nManager.localeIdentifier;
+      }
+      setLanguage(deviceLanguage);
+    };
+
+    getDeviceLanguage();
+  }, []);
+
+  return language;
+};
